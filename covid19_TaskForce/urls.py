acac3dd672b98318acc5d189d1bb5c9a23621547
Nikhil <shubhanshuagrawal0608@gmail.com>
2021-03-01 02:25:45 +0530

    add

--- covid19_TaskForce/urls.py
+++ covid19_TaskForce/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('output',views.output,name='output'),
+    
     path('index', views.index, name="home"),
     path('shedule',views.output, name='shedule')
 ]
