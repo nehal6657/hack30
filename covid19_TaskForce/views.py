18c611823df534175a140ead1dc464688b3be337
nehal6657 <nehalreshuaaryan@gmail.com>
2021-03-01 00:06:13 +0530

    a:

--- covid19_TaskForce/views.py
+++ covid19_TaskForce/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):    
     return render(request, 'index.html')
+
+def output(request):
+    total = (request.GET['total'])
+    startdate = (request.GET['startdate'])
+    noofquarantine = (request.GET['noofquarantine'])
+    nonq = (request.GET['nonq'])
+    room = (request.GET['room'])
+    qt = (request.GET['qt'])
+
+    print(total,startdate,noofquarantine,nonq)
+
+    return render(request, 'output.html')
